c2cf8c99d279d89e0aea21a51bf9cba5a0d66113
Anwesh43 <anweshthecool0@gmail.com>
2018-10-29 10:04:35 +0530

    adding fullscreen support

--- app/src/main/java/com/anwesh/uiprojects/linkedtalllinesstepview/MainActivity.kt
+++ app/src/main/java/com/anwesh/uiprojects/linkedtalllinesstepview/MainActivity.kt
@@ -2,6 +2,7 @@ package com.anwesh.uiprojects.linkedtalllinesstepview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
 import com.anwesh.uiprojects.talllinesstepview.TAllLinesStepView
 
 class MainActivity : AppCompatActivity() {
@@ -9,5 +10,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         TAllLinesStepView.create(this)
+        fullScreen()
     }
+}
+
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
