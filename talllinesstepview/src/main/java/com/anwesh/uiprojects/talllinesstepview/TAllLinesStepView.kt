605f358d8cf217f41015b02149be942770601c0a
Anwesh43 <anweshthecool0@gmail.com>
2018-10-29 09:21:03 +0530

    adding drawTALSNode canvas extension method

--- talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
+++ talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
@@ -13,6 +13,37 @@ import android.content.Context
 
 val nodes : Int = 5
 
+val purpleColor : Int = Color.parseColor("#3F51B5")
+
+val lines : Int = 3
+
+fun Paint.setPurpleThickLine(w : Float, h : Float) {
+    strokeWidth = Math.min(w, h) / 60
+    color = purpleColor
+    strokeCap = Paint.Cap.ROUND
+}
+
+fun Canvas.drawTALSNode(i : Int, scale : Float, paint : Paint) {
+    val w : Float = width.toFloat()
+    val h : Float = height.toFloat()
+    val gap : Float = h / (nodes + 1)
+    val size : Float = gap / 3
+    val osc : Float = 1f / lines
+    paint.setPurpleThickLine(w, h)
+    save()
+    translate(w/2, gap + gap * i)
+    for (j in 0..2) {
+        val sc : Float = Math.min(osc, Math.max(0f, scale - osc * j)) * 3
+        val lSize : Float = size * (1 + j % 2)
+        save()
+        translate(0f, -size/2)
+        rotate(90f * j)
+        drawLine(0f, 0f, lSize, 0f, paint)
+        restore()
+    }
+    restore()
+}
+
 class TAllLinesStepView(ctx : Context) : View(ctx) {
 
     private val paint : Paint = Paint(Paint.ANTI_ALIAS_FLAG)
