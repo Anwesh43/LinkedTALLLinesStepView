c31888508017bddb6d227ce462f84a368ad9993d
Anwesh43 <anweshthecool0@gmail.com>
2018-10-29 09:09:33 +0530

    adding talllinestepview module

--- talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
+++ talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
@@ -3,3 +3,12 @@ package com.anwesh.uiprojects.talllinesstepview
 /**
  * Created by anweshmishra on 29/10/18.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+import android.content.Context
+
+val nodes : Int = 5
