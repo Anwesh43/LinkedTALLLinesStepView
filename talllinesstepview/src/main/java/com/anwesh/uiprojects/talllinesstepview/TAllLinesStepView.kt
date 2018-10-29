e2627682444cc47d4991ee32de2ced2e2c694dd2
Anwesh43 <anweshthecool0@gmail.com>
2018-10-29 09:44:13 +0530

    add TALSNode class

--- talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
+++ talllinesstepview/src/main/java/com/anwesh/uiprojects/talllinesstepview/TAllLinesStepView.kt
@@ -108,4 +108,49 @@ class TAllLinesStepView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class TALSNode(var i : Int, val state : State = State()) {
+
+        private var prev : TALSNode? = null
+
+        private var next : TALSNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor(){
+            if (i < nodes - 1) {
+                next = TALSNode(i + 1)
+                next?.prev = this
+            }
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawTALSNode(i, state.scale, paint)
+            prev?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : TALSNode {
+            var curr : TALSNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this
+        }
+    }
 }
